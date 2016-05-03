109457c694e91692238f643eb1812630f43f487e
Tracy Royal <TracyRoyal@mac.com>
2016-05-03 14:46:46 -0400

    fix production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
    #domain:            'heroku.com'
    #authentication:    :plain,
    #content_type:      'text/html'
- }
+ #}
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
